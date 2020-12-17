4e029c00141b66e32bf17d08e371f9b27be2469f
ianrios <ian.rios1a@gmail.com>
2020-12-17 17:32:55 -0500

    updated level data

--- src/data/levelData.js
+++ src/data/levelData.js
@@ -1,12 +1,14 @@
+export const moveTypes = ['forward', 'left', 'right', 'jump', 'light', 'f1', 'f2']
+export const blockTypes = ['walk', 'light']
 export const levels = [
     {
         "id": 0,
         "section_id": 0,
-        "name": "Level 1",
         "level_data": [
-            { "x": 0, "y": 0, "z": 0, "type": "walk" },
-            { "x": 1, "y": 0, "z": 0, "type": "walk" },
-            { "x": 2, "y": 0, "z": 0, "type": "light" }
-        ]
+            { "x": 0, "y": 0, "z": 0, "type": blockTypes[0] },
+            { "x": 1, "y": 0, "z": 0, "type": blockTypes[0] },
+            { "x": 2, "y": 0, "z": 0, "type": blockTypes[1] }
+        ],
+        "available_blocks": [moveTypes[0], moveTypes[4]]
     }
 ]
