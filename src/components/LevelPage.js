a2208e211e026ae2ad29cd6faf806ec211213922
ianrios <ian.rios1a@gmail.com>
2020-12-21 09:58:10 -0700

    added x and y coordinates

--- src/components/LevelPage.js
+++ src/components/LevelPage.js
@@ -10,11 +10,58 @@ function LevelPage() {
             <Stage width={window.innerWidth} height={window.innerHeight}>
                 <Layer>
                     <Cube
-                        xPosition={0}
-                        yPosition={0}
+                        x={0}
                         zPosition={0}
                         handleClick={e => console.log('click')}
                     />
+                    <Cube
+                        x={1}
+                        zPosition={0}
+                        handleClick={e => console.log('click')}
+                    />
+                    <Cube
+                        x={2}
+                        zPosition={0}
+                        lightable={true}
+                        handleClick={e => console.log('click')}
+                    />
+
+                    <Cube
+                        x={3}
+                        y={2}
+                        zPosition={0}
+                        handleClick={e => console.log('click')}
+                    />
+                    <Cube
+                        x={3}
+                        y={1}
+                        zPosition={0}
+                        handleClick={e => console.log('click')}
+                    />
+                    <Cube
+                        x={3}
+                        y={0}
+                        zPosition={0}
+                        handleClick={e => console.log('click')}
+                    />
+                    <Cube
+                        x={4}
+                        y={0}
+                        handleClick={e => console.log('click')}
+                    />
+                    <Cube
+                        x={5}
+                        y={0}
+                        handleClick={e => console.log('click')}
+                    />
+                    
+                    <Cube
+                        x={6}
+                        y={0}
+                        z={1}
+                        handleClick={e => console.log('click')}
+                    />
+                    
                 </Layer>
             </Stage>
         </div>
--- src/components/sprites/Cube.js
+++ src/components/sprites/Cube.js
@@ -10,32 +10,49 @@ const colors = {
     'yellow': "#F8D525"
 }
 
+const scaleX = .5;
+const scaleY = .5;
+const width = 160
+const height = 80
+function Cube({ x = 0, y = 0, xPosition = 0, yPosition = 0, zPosition = 0, handleClick, lightable = false }) {
+    // const moveX = xPosition * .5
+    // const moveY = yPosition * .5
 
-
-function Cube({ xPosition, yPosition, zPosition, handleClick }) {
+    const moveX = (x * .25) + (y * .25)
+    const moveY = (x * .25) - (y * .25)
+    const multiplyZ = 0
     return (
         <>
             <Shape
                 sceneFunc={(context, shape) => {
                     context.beginPath();
-                    context.moveTo(170, 50);
-                    context.lineTo(90, 10);
-                    context.lineTo(10, 50);
-                    context.lineTo(90, 90);
+                    context.moveTo(
+                        (scaleX * (width + 10)) + (moveX * width),
+                        (scaleY * ((height / 2) + 10)) + (moveY * height));
+                    context.lineTo(
+                        (scaleX * ((width / 2) + 10)) + (moveX * width),
+                        (scaleY * (0 + 10)) + (moveY * height));
+                    context.lineTo(
+                        (scaleX * (0 + 10)) + (moveX * width),
+                        (scaleY * ((height / 2) + 10)) + (moveY * height));
+                    context.lineTo(
+                        (scaleX * ((width / 2) + 10)) + (moveX * width),
+                        (scaleY * (height + 10)) + (moveY * height));
                     context.closePath();
                     context.fillStrokeShape(shape);
                 }}
-                fill={colors['lighterGrey']}
+                fill={lightable ? colors['blue'] : colors['lighterGrey']}
                 stroke={colors['darkGrey']}
                 strokeWidth={2}
             />
             <Shape
                 sceneFunc={(context, shape) => {
                     context.beginPath();
-                    context.moveTo(170, 50);
-                    context.lineTo(170, 100);
-                    context.lineTo(90, 140);
-                    context.lineTo(90, 90);
+
+                    context.moveTo((scaleX * 170) + (moveX * width), (scaleY * 50) + (moveY * height));
+                    context.lineTo((scaleX * 170) + (moveX * width), (scaleY * 100) + (moveY * height));
+                    context.lineTo((scaleX * 90) + (moveX * width), (scaleY * 140) + (moveY * height));
+                    context.lineTo((scaleX * 90) + (moveX * width), (scaleY * 90) + (moveY * height));
                     context.closePath();
                     context.fillStrokeShape(shape);
                 }}
@@ -46,10 +63,11 @@ function Cube({ xPosition, yPosition, zPosition, handleClick }) {
             <Shape
                 sceneFunc={(context, shape) => {
                     context.beginPath();
-                    context.moveTo(10, 50);
-                    context.lineTo(10, 100);
-                    context.lineTo(90, 140);
-                    context.lineTo(90, 90);
+
+                    context.moveTo((scaleX * 10) + (moveX * width), (scaleY * 50) + (moveY * height));
+                    context.lineTo((scaleX * 10) + (moveX * width), (scaleY * 100) + (moveY * height));
+                    context.lineTo((scaleX * 90) + (moveX * width), (scaleY * 140) + (moveY * height));
+                    context.lineTo((scaleX * 90) + (moveX * width), (scaleY * 90) + (moveY * height));
                     context.closePath();
                     context.fillStrokeShape(shape);
                 }}
